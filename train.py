de7d92188e029034e8cb6a572a3cf222c2855eed
Yamaguchi-yushi <pxef5shw@s.okayama-u.ac.jp>
2026-06-12 14:27:48 +0900

    path

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import os
 """
 command = [
     ["python3", "test.py", "map_5x4", "3", "pbs", "tp"]
@@ -17,12 +18,14 @@ for i in range(num_runs):
     #algとmap，実行step数確認，drp_envのpbs用の変更箇所
     #iql,aoba00,16050000,unsafe
     command = (
-        f'python src/epymarl/src/main.py --config=qmix --env-config=gymma '
+        f'python src/epymarl/src/main.py --config=mappo --env-config=gymma '
         f'with env_args.time_limit=500 '
-        f'env_args.key="drp_env:drp_safe-5agent_map_aoba00-v2" '
+        f'env_args.key="drp_env:drp_safe-4agent_map_8x5-v2" '
         f'env_args.state_repre_flag="onehot_fov" '
         )
-    proc = subprocess.Popen(command, shell=True)
+    env = os.environ.copy()
+    env.pop('PYTHONPATH', None)
+    proc = subprocess.Popen(command, shell=True, env=env)
     running_processes.append(proc)
 
     while len(running_processes) >= maxpurocesses:
